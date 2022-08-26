1a5d8de0777d4df7f9a40022d08773307feef310
poojayadhav567 <92743586+poojayadhav567@users.noreply.github.com>
2022-08-26 13:27:43 +0530

    Delete area.py

--- area.py
+++
@@ -1,6 +0,0 @@
-import math as m
-print("computing the area of the circle")
-rad=float(input("Enter the radius of the circle :" ))
-area=m.pi*rad*rad
-print("The radius of the circle is :",rad)
-print("The area of the circle is: ",area)
